8fc191f8cd53436b9829125fc55b1757b5fa4622
zichen.wang <zichen.wang@mthreads.com>
2026-05-27 16:53:54 +0800

    mod tensor schema to mutable alias

--- csrc/musa/torch_bindings.cpp
+++ csrc/musa/torch_bindings.cpp
@@ -50,7 +50,7 @@ TORCH_LIBRARY_EXPAND(CONCAT(TORCH_EXTENSION_NAME, _musa_ops), musa_ops) {
   musa_ops.impl("silu_and_mul_per_token_group_fp8_quant", torch::kMUSA,
                 &silu_and_mul_per_token_group_fp8_quant);
   musa_ops.def(
-      "musa_top_k_top_p_sampling_from_probs(Tensor probs, Tensor output, Tensor? maybe_indices, Tensor? "
+      "musa_top_k_top_p_sampling_from_probs(Tensor probs, Tensor! output, Tensor? maybe_indices, Tensor? "
       "maybe_top_k_arr, "
       "float top_k_val, Tensor? maybe_top_p_arr, float top_p_val, bool deterministic, Generator? gen) -> ()");
   musa_ops.impl("musa_top_k_top_p_sampling_from_probs", torch::kMUSA, &musa_top_k_top_p_sampling_from_probs);
@@ -66,12 +66,12 @@ TORCH_LIBRARY_EXPAND(CONCAT(TORCH_EXTENSION_NAME, _musa_ops), musa_ops) {
   musa_ops.impl("top_p_renorm_probs", torch::kMUSA, &top_p_renorm_probs);
 
   musa_ops.def(
-      "min_p_sampling_from_probs(Tensor probs, Tensor output, Tensor? maybe_indices, Tensor? maybe_min_p_arr, float "
+      "min_p_sampling_from_probs(Tensor probs, Tensor! output, Tensor? maybe_indices, Tensor? maybe_min_p_arr, float "
       "min_p_val, bool deterministic, Generator? gen) -> ()");
   musa_ops.impl("min_p_sampling_from_probs", torch::kMUSA, &min_p_sampling_from_probs);
 
   musa_ops.def(
-      "top_p_sampling_from_probs(Tensor probs, Tensor output, Tensor? maybe_indices, Tensor? maybe_top_p_arr, "
+      "top_p_sampling_from_probs(Tensor probs, Tensor! output, Tensor? maybe_indices, Tensor? maybe_top_p_arr, "
       "float top_p_val, bool deterministic, Generator? gen) -> ()");
   musa_ops.impl("top_p_sampling_from_probs", torch::kMUSA, &top_p_sampling_from_probs);
 #endif
