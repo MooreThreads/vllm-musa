2fcae39386caa70204bfb7c93702f4d2080c23f7
Xiaodong Ye <yexiaodong60@mthreads.com>
2026-07-28 16:22:45 +0800

    perf(musa): fuse Qwen next-token input updates

--- vllm_musa/v1/worker/__init__.py
+++ vllm_musa/v1/worker/__init__.py
@@ -1,3 +1,4 @@
+from vllm_musa.v1.worker import qwen_fused_next_input_ids as qwen_fused_next_input_ids
 from vllm_musa.v1.worker import qwen_identity_logits_view as qwen_identity_logits_view
 
-__all__ = ["qwen_identity_logits_view"]
+__all__ = ["qwen_fused_next_input_ids", "qwen_identity_logits_view"]
