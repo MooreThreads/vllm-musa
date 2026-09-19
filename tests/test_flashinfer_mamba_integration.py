3b657ce6b9a594090ab318b892b1afc0a38015e4
Xiaodong Ye <yexiaodong60@mthreads.com>
2026-09-19 17:38:26 +0800

    pin: track tensor MTP recurrence

--- tests/test_flashinfer_mamba_integration.py
+++ tests/test_flashinfer_mamba_integration.py
@@ -22,7 +22,7 @@ def test_flashinfer_pin_targets_musa_nemotron_fork() -> None:
     pins = _pins()
     assert pins["FLASHINFER_REPOSITORY"] == "https://github.com/yeahdongcn/flashinfer.git"
     assert re.fullmatch(r"[0-9a-f]{40}", pins["FLASHINFER_COMMIT"])
-    assert pins["FLASHINFER_COMMIT"] == "233e437de4e6dfc097cde0b0ca4622f7dd1c6928"
+    assert pins["FLASHINFER_COMMIT"] == "a5597b4ce3c6c5864bd6931a03369dc3bf697a32"
 
 
 def test_setup_overlays_pinned_mamba_provider() -> None:
